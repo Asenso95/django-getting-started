c3c0cdce77ef13235b2bb360dd978c51bd9bc28d
Ben Asenso <asenso95@gmail.com>
2025-03-29 00:55:01 +0000

    Update wsgi.py

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -10,6 +10,8 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 import os
 
 import django
+from django.contrib.auth import get_user_model
+
 
 from django.core.wsgi import get_wsgi_application
 
@@ -18,6 +20,25 @@ django.setup()
 
 from django.core.management import call_command
 call_command('migrate')  # This runs migrations automatically
+
+
+# Create superuser automatically
+User = get_user_model()
+
+SUPERUSER_USERNAME = "admin"
+SUPERUSER_EMAIL = "admin@example.com"
+SUPERUSER_PASSWORD = "AdminPassword123"
+
+if not User.objects.filter(username=SUPERUSER_USERNAME).exists():
+    User.objects.create_superuser(
+        username=Admin,
+        email= admin@gmail.com,
+        password=adminpassword 
+    )
+    print("Superuser created successfully!")
+else:
+    print("Superuser already exists!")
+
 
 
 application = get_wsgi_application()
